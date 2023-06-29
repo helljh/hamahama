734e3d226a636e42b27a353f54049d39cd8ba9f0
helljh <junho308917@naver.com>
2023-06-29 18:27:03 +0900

    허준호 수정

--- src/JS/App.js
+++ src/JS/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Login from '../Screen/Login';
-import Auth from '../Screen/Auth';
+import MenuBar from '../JS/MenuBar';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 const App = () => {
   return (
-    <div >
-      <Login/>
-      <Auth/>
+    <div>
+      {/* <Login/> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/"  exact element={<Login/>}></Route>
+          <Route path="/main"  element={<MenuBar/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
+
 
 export default App;
--- src/Screen/Login.js
+++ src/Screen/Login.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import classes from '../css/Login.module.css';
+import Auth from '../Screen/Auth';
+import {useNavigate} from 'react-router-dom';
 
 const Login = () => {
 
+    const navigate = useNavigate();
+    const navigateToMain = () => {
+        navigate("/main");
+    }
     const LoginHandler = (e) => {
         e.preventDefault();
     }
@@ -16,27 +22,30 @@ const Login = () => {
     }
 
     return (
-        <main className={classes.login}>
-            <section>
-                <h2 className={classes.spacing}>헐레벌떡</h2>
-                <form onSubmit={LoginHandler}>
-                    <div className={classes.control}>
-                        <input type='email' placeholder='전화번호, 사용자 이름, 또는 이메일' onChange={emailChangeHandler} />
-                        <input type='password' placeholder='비밀번호' onChange={passwordChangeHandler} />
+        <div>
+            <div className={classes.login}>
+                <section>
+                    <h2 className={classes.spacing}>헐레벌떡</h2>
+                    <form onSubmit={LoginHandler}>
+                        <div className={classes.control}>
+                            <input type='email' placeholder='전화번호, 사용자 이름, 또는 이메일' onChange={emailChangeHandler} />
+                            <input type='password' placeholder='비밀번호' onChange={passwordChangeHandler} />
+                        </div>
+                        <button className={classes.button} onClick={navigateToMain}>로그인</button>
+                    </form>
+                    <div className={classes.hr}>또는</div>
+                    <div className={classes.kakaoButton}>
+                        <img className={classes.img} src='img/kakao.png' alt='kakao'></img>
+                        <a href="#">카카오톡으로 로그인</a>
                     </div>
-                    <button className={classes.button}>로그인</button>
-                </form>
-                <div className={classes.hr}>또는</div>
-                <div className={classes.kakaoButton}>
-                    <img className={classes.img} src='img/kakao.png' alt='kakao'></img>
-                    <a href="#">카카오톡으로 로그인</a>
-                </div>
-                <div className={classes.forgotPwd}>
-                    <a href="#">비밀번호를 잊으셨나요?</a>
-                </div>
-            </section>
-            
-        </main>
+                    <div className={classes.forgotPwd}>
+                        <a href="#">비밀번호를 잊으셨나요?</a>
+                    </div>
+                </section>
+            </div>
+            <Auth/>
+        </div>
+       
         
         
     );
