0f2b5c11f9869717a4d5971193cfdb7a0649930a
kdw0737 <113456098+kdw0737@users.noreply.github.com>
2023-07-04 22:53:29 +0900

    Add files via upload

---
+++ huldduck/src/main/java/Idle/huldduck/HuldduckApplication.java
@@ -0,0 +1,13 @@
+package Idle.huldduck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuldduckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuldduckApplication.class, args);
+	}
+
+}
---
+++ huldduck/src/test/java/Idle/huldduck/HuldduckApplicationTests.java
@@ -0,0 +1,13 @@
+package Idle.huldduck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuldduckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
